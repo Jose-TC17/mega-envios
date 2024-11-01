56ad375019580f275fde07ba0771cf7e6e233a9e
Jose-TC17 <https://github.com/Jose-TC17>
2024-11-01 15:00:45 -0500

    verificando error

--- src/data/SectionSends.ts
+++ src/data/SectionSends.ts
@@ -3,8 +3,8 @@ import imgMapPeruLima from "../img/MapaPeruLima.png"
 
 
 const sectionSends: string[][] = [
-  ["enviosLima", "Envíos a todo Lima - Callao", imgMapPeruLima, "/envios-lima", ],
-  ["enviosProvincia", "Envíos a Provincias", imgMapPeru, "/envios-provinciales"],
+  ["enviosLima", "Envíos a todo Lima - Callao", imgMapPeruLima, "./envios-lima", ],
+  ["enviosProvincia", "Envíos a Provincias", imgMapPeru, "./envios-provinciales"],
 ];
 
 
